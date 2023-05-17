7f0d3e9f4c6c2eb7396523ccb2be2c5fa50a7728
JellyGh0st <vsumerkach@gmail.com>
2023-05-17 17:28:28 +0300

    first setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import './App.css';
-import 'normalize.css';
-import { Routes, Route} from 'react-router-dom';
-
-
+import "./App.css";
+import "normalize.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-  <>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Hero/>} />
-      <Route path="/login" element={<Login/>} />
-      
-    </Routes>
-    <Footer/>
-  </>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/userPage" element={<UserPage />} />
+        <Route path="/forum" element={<Forum />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
