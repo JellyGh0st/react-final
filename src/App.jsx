dea36751c4c4f27e6dbdea07e8db99ba958ca10a
JellyGh0st <vsumerkach@gmail.com>
2023-05-18 23:22:30 +0300

    design theme test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import "./App.css";
-import "normalize.css";
+
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/UI/Header";
+import Footer from "./components/UI/Footer";
+import Landing from "./components/Pages/Landing";
+import Login from "./components/Pages/Login";
+import Register from "./components/Pages/Register";
+import UserPage from "./components/Pages/UserPage";
+import Forum from "./components/Pages/Forum";
+import About from "./components/Pages/About";
+import Contact from "./components/Pages/Contact";
+
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route index element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/userPage" element={<UserPage />} />
--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,6 +1,20 @@
+
+
 const Header = () => {
-   return ( <>
-   </>  );
+   return (
+      
+   <header>
+      <div className="header-nav">
+         <div className="header-logo">
+            <h1>BANDZOO</h1>
+         </div>
+         <div className="header-buttons">
+            <button>Log In</button>
+            <button>Sign In</button>
+         </div>
+      </div>
+   </header>
+     );
 }
  
 export default Header;
